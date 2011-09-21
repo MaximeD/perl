d726854ecec343ca42e2623d759989f698cc8a6e
Maxime Demolin <akbarova.armia@gmail.com>
2011-09-21 20:48:22 +0200

    Perl helper for treetagger

---
+++ treetagger.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+ 
+# Simple interface for TreeTagger
+# You will need the original treetagger :
+# http://www.ims.uni-stuttgart.de/projekte/corplex/TreeTagger/
+
+# TODO :
+# modify every weird stuff for TreeTagger
+# like a " following a '
+
+########################
+#  Available langages  #
+########################
+chomp(@lang = qx (ls lib/*.par)) ;
+
+$lang = join(" ",@lang) ;
+$lang =~ s/lib\///gi;
+$lang =~ s/\.par//gi;
+
+print "Installed languages are : $lang\n" ;
+print "Please enter the original language of the text :\n";
+chomp($text_lang = <STDIN>) ;
+###########################################################
+
+
+
+##########
+# export #
+##########
+open (TAGGED, '>>pouet.txt');      # original file
+open (UNKNOWN, '>>unknown.txt');   # every unknown
+###########################################################
+
+#################
+#  TreeTagging  #
+#################
+@file = <> ;
+@tag = qx (echo '@file' | cmd/tree-tagger-$text_lang) ;
+print TAGGED @tag ;
+
+@unknown = grep(/unknown/, @tag);
+print UNKNOWN @unknown ;
+
+
