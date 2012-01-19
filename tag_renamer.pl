a019948d58381018a17c72f7d784302540af0967
Maxime Demolin <akbarova.armia@gmail.com>
2012-01-19 15:44:37 +0100

    rewrite +add ogg support

--- tag_renamer.pl
+++ tag_renamer.pl
@@ -7,59 +7,68 @@ use MP3::Tag; # you'll probably to install this one
 
 # first, go to the right dir
 foreach my $dir (@ARGV) {
-    chdir $dir or die ;
+    chdir $dir or die "$!\n";
 }
 
-# get list of mp3 infos
 my @mp3s ;
 my ($title, $track, $artist, $album, $comment, $year, $genre);
-&tag("get_infos") ;
 
-# print'em
-print "$artist/$year. $album ($genre)\n";
+my @files = glob("*.{mp3,ogg}");
 
+foreach (@files) {
+  my $mp3 = MP3::Tag->new($_);
+  ($title, $track, $artist, $album, $comment, $year, $genre) = $mp3->autoinfo();
+  $_ =~ /\.(?<extension>[^.]*)$/ ;
+  my $extension = $+{extension} ;
+
+  # clean track number
+  $track =~ s/(\d+?)\/\d+/$1/ ;
+  $track =~ s/^(\d)$/0$1/ if ($track =~ /^(\d)$/);
+
+  $mp3->close();
+
+  push @mp3s, {
+	       name       => $_ ,
+	       title      => $title,
+	       track      => $track,
+	       artist     => $artist,
+	       album      => $album,
+	       year       => $year,
+	       genre      => $genre,
+	       extension  => $extension
+	      } ;
+}
+
+
+my $album_info = $artist . "/" . $year . ". " . $album;
+$album_info .= " (" . $genre .")" if $genre ne "";
+print "$album_info\n";
+
+my $need_to_change = 0 ;
 for my $mp3(@mp3s) {
-    print "\t$mp3->{track}. $mp3->{title}\n";
+  my $new_name = "$mp3->{track}. $mp3->{title}.$mp3->{extension}" ;
+
+  if ($mp3->{name} ne $new_name) {
+    $need_to_change = 1;
+    printf "\t%s %90s\n", $new_name, "<- $mp3->{name}";
+  }
+  else {
+    print "\t$new_name\n";
+  }
 }
 
-# ask if it is ok to rename
-print "\nRename files according to this ?\n";
-if (<STDIN> =~ /^y/) {
-    &tag("rename") ;
-    print "Done!\n";
-}
-
-else {
-    die "Not doing anything\n";
-}
-
-sub tag {
-    while (<*.mp3>) {
-	my $mp3 = MP3::Tag->new($_);
-	($title, $track, $artist, $album, $comment, $year, $genre) = $mp3->autoinfo();
-
-	# clean track number
-	$track =~ s/(\d+?)\/\d+/$1/ ;
-
-	if ($_[0] eq "get_infos"){
-	    push @mp3s, {  title   => $title,
-			   track   => $track,
-			   artist  => $artist,
-			   album   => $album,
-			   year    => $year,
-			   genre   => $genre
-	    } ;
-	}
-
-	if ($_[0] eq "rename"){
-	    # I like tags to be like the following : "track. name"
-	    my $new_name = $track . ". " . $title . ".mp3";
-
-	    # Rename the files
-	    move($_, $new_name);
-	    
-	    # now remove comments
-	    $mp3->close();
-	}
+# ask if it is needed and ok to rename
+if ($need_to_change == 1) {
+  print "\nRename files according to this ?\n";
+  if (<STDIN> =~ /^y/) {
+    for my $mp3(@mp3s) {
+      my $new_name = "$mp3->{track}. $mp3->{title}.$mp3->{extension}" ;
+      move($mp3->{name}, $new_name);
     }
+    print "Done!\n";
+  }
+
+  else {
+    die "Not doing anything\n";
+  }
 }
